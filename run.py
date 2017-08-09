ca247ce6f3609cc02857d2659f7134e4fbed0711
gogoforit <715157026@qq.com>
2017-08-09 11:54:27 +0800

    完成添加备注

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import os
+import time
+import datetime
 from mongoengine import *
 from mongoengine.context_managers import switch_db
 from flask import flash
-from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask import render_template
-from flask import request
+from flask import Flask, Response, redirect, url_for, request, session, abort, render_template
+from flask.ext.login import LoginManager, UserMixin, \
+    login_required, login_user, logout_user
 
 local_username = os.environ['LOCAL_USERNAME']
 local_password = os.environ['LOCAL_PASSWORD']
@@ -15,6 +17,8 @@ remote_username = os.environ['REMOTE_USERNAME']
 remote_password = os.environ['REMOTE_PASSWORD']
 remote_host = os.environ['REMOTE_HOST']
 remote_db = os.environ['REMOTE_DB']
+remarks_username = os.environ['REMARKS_USERNAME']
+remarks_password = os.environ['REMARKS_PASSWORD']
 
 connect(db=remote_db,
         host=remote_host,
@@ -28,13 +32,29 @@ connect(db=local_db,
         password=local_password,
         alias='local_db')
 
-
-app_secret_key = os.environ['APP_SECRET_KEY']
 app = Flask(__name__)
 app.config.from_object(__name__)
 bootstrap = Bootstrap(app)
-app.config['SECRET_KEY'] = 'hard to guess'
-app.secret_key = app_secret_key
+
+# config
+app.config.update(
+    DEBUG=True,
+    SECRET_KEY='secret_xxx'
+)
+
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+users = User('wxwifisign')
 
 
 def get_macs():
@@ -66,6 +86,36 @@ def get_pid():
     k = k[2].split(' ')
     pid = k[0]
     return pid
+
+def get_now_datetime():
+    now_hour = time.strftime('%H')
+    now_min = time.strftime('%M')
+    now_sec = time.strftime('%S')
+    now_time = datetime.time(int(now_hour), int(now_min), int(now_sec))
+    return now_time
+
+def get_class_num():
+    now_time = get_now_datetime()
+    info = {}
+    class_time_start_1 = datetime.time(8, 0, 0)
+    class_time_end_1 = datetime.time(10, 0, 0)
+    class_time_start_2 = datetime.time(10, 5, 0)
+    class_time_end_2 = datetime.time(12, 0, 0)
+    class_time_start_3 = datetime.time(14, 30, 0)
+    class_time_end_3 = datetime.time(16, 0, 0)
+    class_time_start_4 = datetime.time(16, 0, 0)
+    class_time_end_4 = datetime.time(18, 0, 0)
+    if now_time > class_time_start_1 and now_time < class_time_end_1:
+        info['class_num'] = '1'
+    elif now_time > class_time_start_2 and now_time < class_time_end_2:
+        info['class_num'] = '2'
+    elif now_time > class_time_start_3 and now_time < class_time_end_3:
+        info['class_num'] = '3'
+    elif now_time > class_time_start_4 and now_time < class_time_end_4:
+        info['class_num'] = '4'
+    else:
+        info['class_num'] = '5'
+    return info['class_num']
 
 
 @app.route('/')
@@ -159,5 +209,68 @@ def modify_information():
             flash('修改成功！')
     return render_template('modify_information.html')
 
+
+# somewhere to login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if password == remarks_password and username == remarks_username:
+            id = remarks_username
+            user = User(id)
+            login_user(user)
+            return redirect(url_for('remarks'))
+        else:
+            return abort(401)
+    else:
+        return render_template('login.html')
+
+
+@app.route('/remarks', methods=['GET', 'POST'])
+@login_required
+def remarks():
+    from moduls.remarks import Remarks
+    if request.method == 'GET':
+        return render_template('remarks.html')
+    else:
+        with switch_db(Remarks, 'local_db') as Remarks:
+            if Remarks.objects(class_id=request.form['class_id'],
+                               class_num=get_class_num()):
+                remarks_info = Remarks.objects(class_id=request.form['class_id'],
+                                          class_num=get_class_num()).first()
+                remarks_info['remarks'] = request.form['remarks']
+                remarks_info.save()
+                flash('修改成功')
+                return render_template('remarks.html')
+            else:
+                remarks = Remarks(class_id=request.form['class_id'],
+                                  class_num=get_class_num(),
+                                  remarks=request.form['remarks'])
+                remarks.save()
+                flash('设置成功')
+                return render_template('remarks.html')
+
+
+# somewhere to logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return Response('<p>Logged out</p>')
+
+
+# handle login failed
+@app.errorhandler(401)
+def page_not_found(e):
+    return Response('<p>Login failed</p>')
+
+
+# callback to reload the user object
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8899)
